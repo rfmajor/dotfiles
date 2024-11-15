d4413e22a7b21233deed1a6f69f11439d1ef7c1e
rfmajor <rfmajor99@gmail.com>
2024-11-15 13:55:23 +0100

    Add some more mappings to telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,3 +7,13 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end) 
+vim.keymap.set('n', '<leader>ls', builtin.live_grep, {})
+
+-- put the highlighted text into the search buffer
+vim.keymap.set('x', '<leader>ls', '\"zy:Telescope live_grep default_text=<C-r>z<cr>')
+-- resume last search
+vim.keymap.set('n', '<leader>rs', require('telescope.builtin').resume, {
+    noremap = true,
+    silent = true,
+    desc = 'Resume',
+})
