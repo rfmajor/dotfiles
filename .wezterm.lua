14a6eeb08ae63e7a92d550fccb20708199876dff
rfmajor <rfmajor99@gmail.com>
2024-12-30 10:53:15 +0100

    Fix the wezterm script

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,7 @@ config.color_scheme = 'Elementary'
 config.background = {
     {
         source = {
-            File = os.getenv("HOME") + '/dotfiles/wallpaper.png',
+            File = os.getenv("HOME") .. '/dotfiles/wallpaper.png',
         },
         width = "100%",
         hsb = dimmer,
