c7a00d17753c084c79d504cbd0b6ddfa6450472c
rfmajor <rfmajor99@gmail.com>
2024-12-30 10:37:54 +0100

    Add wezterm config file

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+-- config.color_scheme = 'Bright Lights'
+-- config.color_scheme = 'Ciapre'
+config.color_scheme = 'Elementary'
+-- config.color_scheme = 'Framer'
+---config.color_scheme = 'JetBrains Darcula'
+
+config.background = {
+    {
+        source = {
+            File = '/home/fmajor/dotfiles/wallpaper.png',
+        },
+        width = "100%",
+        hsb = dimmer,
+    }
+}
+
+return config
