cd209765ff2b529686fbe84641187a90e5710761
rfmajor <rfmajor99@gmail.com>
2025-01-08 16:02:50 +0100

    Improve changing color schemes, fix cursor

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,14 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+
 default_color_scheme = 'Bright Lights'
 color_schemes = {'Bright Lights', 'Framer', 'Catppuccin Macchiato', 'Catppuccin Mocha', 'Chalk', 'Chalkboard', 'Chester'}
 current_scheme_idx = 1
 config.color_scheme = default_color_scheme
+config.colors = {}
+config.colors.cursor_bg = 'white'
+config.colors.cursor_fg = 'black'
+
 config.harfbuzz_features = { 'calt=0' }
 config.enable_tab_bar = false
 config.background = {
@@ -18,25 +23,25 @@ config.background = {
 function change_colors(window, pane, start, inc)
     local overrides = window:get_config_overrides() or {}
     current_scheme_idx = current_scheme_idx + inc
-    if color_schemes[current_scheme_idx] == default_color_scheme then
-        current_scheme_idx = current_scheme_idx + inc
-    end
     if current_scheme_idx > #color_schemes or current_scheme_idx < 1 then
-        wezterm.log_info("Changing color scheme to " .. default_color_scheme)
-        overrides.color_scheme = nil
         current_scheme_idx = start
-    else
-        wezterm.log_info("Changing color scheme to ", color_schemes[current_scheme_idx])
-        overrides.color_scheme = color_schemes[current_scheme_idx]
-        current_scheme_idx = current_scheme_idx + inc
     end
+
+    if color_schemes[current_scheme_idx] == default_color_scheme then
+        overrides.color_scheme = nil
+        wezterm.log_info("Changing color scheme to " .. default_color_scheme .. " (default)")
+    else
+        overrides.color_scheme = color_schemes[current_scheme_idx]
+        wezterm.log_info("Changing color scheme to " .. color_schemes[current_scheme_idx])
+    end
+
     window:set_config_overrides(overrides)
 end
 wezterm.on('next-colorscheme', function (window, pane)
     change_colors(window, pane, 1, 1)
 end)
 wezterm.on('previous-colorscheme', function (window, pane)
-    change_colors(window, pane, 1, 1)
+    change_colors(window, pane, #color_schemes, -1)
 end)
 config.keys = {
     {
