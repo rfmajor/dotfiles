7ed69b8ca5cc546975c3de2764c524859b9478cd
rfmajor <rfmajor99@gmail.com>
2025-01-19 14:26:01 +0100

    Replace netrw with yazi

--- nvim/lua/fmajor/lazy.lua
+++ nvim/lua/fmajor/lazy.lua
@@ -50,5 +50,38 @@ require("lazy").setup({
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            --{
+            --    "<leader>-",
+            --    mode = { "n", "v" },
+            --    "<cmd>Yazi<cr>",
+            --    desc = "Open yazi at the current file",
+            --},
+            --{
+            --    -- Open in the current working directory
+            --    "<leader>cw",
+            --    "<cmd>Yazi cwd<cr>",
+            --    desc = "Open the file manager in nvim's working directory" ,
+            --},
+            --{
+            --    -- NOTE: this requires a version of yazi that includes
+            --    -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+            --    '<c-up>',
+            --    "<cmd>Yazi toggle<cr>",
+            --    desc = "Resume the last yazi session",
+            --},
+        },
+        ---@type YaziConfig
+        opts = {
+            open_for_directories = true,
+            keymaps = {
+                show_help = '<f1>',
+            },
+        },
+    }
 })
