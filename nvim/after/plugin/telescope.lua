326bb68b6d7d90347f6707771e25a5abfd560a01
rfmajor <rfmajor99@gmail.com>
2025-08-03 14:19:48 +0200

    Remove formatting on save

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,28 +1,26 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>pf', function()
-    builtin.find_files({ hidden = true })
+vim.keymap.set("n", "<leader>pf", function()
+	builtin.find_files({ hidden = true })
 end)
 
-vim.keymap.set('n', '<leader>pj', function()
-    builtin.buffers({ hidden = true })
+vim.keymap.set("n", "<leader>pj", function()
+	builtin.buffers({ hidden = true })
 end)
 
-vim.keymap.set('n', '<leader>ls', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ls", builtin.live_grep, {})
 
 -- put the highlighted text into the search buffer
-vim.keymap.set('x', '<leader>ls', '\"zy:Telescope live_grep default_text=<C-r>z<cr>')
+vim.keymap.set("x", "<leader>ls", '"zy:Telescope live_grep default_text=<C-r>z<cr>')
 
-vim.keymap.set('n', '<C-e>', ":Telescope frecency workspace=CWD theme=ivy<CR>")
+vim.keymap.set("n", "<C-e>", ":Telescope frecency workspace=CWD theme=ivy<CR>")
 
-require('telescope').setup {
-    defaults = {
-    },
-    pickers = {
-        find_files = {
-            theme = "ivy",
-        }
-    },
-    extensions = {
-    }
-}
+require("telescope").setup({
+	defaults = {},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+	},
+	extensions = {},
+})
--- nvim/lua/fmajor/conform.lua
+++ nvim/lua/fmajor/conform.lua
@@ -1,8 +1,8 @@
 require("conform").setup({
-	format_on_save = {
-		timeout_ms = 1000,
-		lsp_fallback = true,
-	},
+	-- format_on_save = {
+	-- 	timeout_ms = 1000,
+	-- 	lsp_fallback = true,
+	-- },
 	formatters_by_ft = {
 		lua = { "stylua" },
 		javascript = { "prettier" },
