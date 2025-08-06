75149b680a0cd0b704f3882a34a98217ef91651f
rfmajor <rfmajor99@gmail.com>
2025-08-06 15:43:22 +0200

    Remove unnecessary wifi popup plugin

--- macos/sketchybar/plugins/wifi_popup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-popup_drawing=$(sketchybar --query wifi | jq -r '.popup.drawing')
-
-if [ "$popup_drawing" = "off" ]; then
-    sketchybar --set wifi popup.drawing=on
-else
-    sketchybar --set wifi popup.drawing=off
-fi
