ad3320b01def11cbd1ee6f387476d2e52544afc7
rfmajor <rfmajor99@gmail.com>
2025-09-03 17:00:48 +0200

    Bind tmux sessionizer to s key, adjust the script

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 switch_to() {
-    if [ -z $TMUX ]; then
-        tmux attach-session -t $1
+    if [ -z "$TMUX" ]; then
+        tmux attach-session -t "$1"
     else
-        tmux switch-client -t $1
+        tmux switch-client -t "$1"
     fi
 }
 
@@ -13,35 +13,35 @@ has_session() {
 }
 
 hydrate() {
-    if [ -f $2/.tmux-sessionizer ]; then
-        tmux send-keys -t $1 "source $2/.tmux-sessionizer" c-M
-    elif [ -f $HOME/.tmux-sessionizer ]; then
-        tmux send-keys -t $1 "source $HOME/.tmux-sessionizer" c-M
+    if [ -f "$2/.tmux-sessionizer" ]; then
+        tmux send-keys -t "$1" "source $2/.tmux-sessionizer" c-M
+    elif [ -f "$HOME/.tmux-sessionizer" ]; then
+        tmux send-keys -t "$1" "source $HOME/.tmux-sessionizer" c-M
     fi
 }
 
 if [ $# -eq 1 ]; then
     selected=$1
 else
-    selected=$(find ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected_name=$(fd . --base-directory ~/ -u -d 1 -t d --format "{/}" | fzf --tmux=70%)
+    selected="$HOME/$selected_name"
 fi
 
-if [ -z $selected ]; then
+if [ -z "$selected_name" ]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-if [ -z $TMUX ] && [ -z $tmux_running ]; then
-    tmux new-session -s $selected_name -c $selected
+if [ -z "$TMUX" ] && [ -z "$tmux_running" ]; then
+    tmux new-session -s "$selected_name" -c "$selected"
 #    hydrate $selected_name $selected
     exit 0
 fi
 
-if ! has_session $selected_name; then
-    tmux new-session -ds $selected_name -c $selected
+if ! has_session "$selected_name"; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 #    hydrate $selected_name $selected
 fi
 
-switch_to $selected_name
+switch_to "$selected_name"
