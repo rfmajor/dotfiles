9686002ef8db0016a3c07ee72cc773d26cef9490
rfmajor <rfmajor99@gmail.com>
2025-09-11 17:58:47 +0200

    Reopen vim at the same position

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,12 @@
 require("fmajor")
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
