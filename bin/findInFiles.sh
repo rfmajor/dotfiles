fa0637e657b1ab430af236b9092eb31daddb8f9a
rfmajor <rfmajor99@gmail.com>
2025-10-10 22:15:34 +0200

    Add find in files script

---
+++ bin/findInFiles.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+choice=$(sk -i -c 'rg -n {} --color=always' --ansi)
+if [ -n "$choice" ]; then
+    filename=$(echo "$choice" | cut --delimiter ':' --fields 1)
+    line=$(echo "$choice" | cut --delimiter ':' --fields 2)
+
+    nvim +"$line" "$filename"
+fi
