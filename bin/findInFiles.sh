5cfa9bb3f7531682575f3233c08533b11408b021
rfmajor <rfmajor99@gmail.com>
2025-10-13 09:36:58 +0200

    Make findInFiles compatible with macOS

--- bin/findInFiles.sh
+++ bin/findInFiles.sh
@@ -2,8 +2,8 @@
 
 choice=$(sk -i -c 'rg -in "{}" --color=always' --ansi)
 if [ -n "$choice" ]; then
-    filename=$(echo "$choice" | cut --delimiter ':' --fields 1)
-    line=$(echo "$choice" | cut --delimiter ':' --fields 2)
+    filename=$(echo "$choice" | cut -d ':' -f 1)
+    line=$(echo "$choice" | cut -d ':' -f 2)
 
     nvim +"$line" "$filename"
 fi
