cec76e2a42d076031c62d140ef5012ac5dbf56d9
rfmajor <rfmajor99@gmail.com>
2025-10-13 12:16:29 +0200

    Use fzf instead of skim, save the last search

--- bin/findInFiles.sh
+++ bin/findInFiles.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-choice=$(sk -i -c 'rg -Fin "{}" --color=always' --ansi)
+LAST_QUERY_CACHE="$HOME/.cache/last_fzf_query"
+RG_PREFIX="rg --column --line-number --smart-case --no-heading --color=always"
+
+choice=$(fzf --bind "start,change:reload:$RG_PREFIX {q} || true" \
+    --bind "enter:execute-silent[echo {q} > $LAST_QUERY_CACHE &]+accept" \
+    --ansi --disabled --query "$(cat "$LAST_QUERY_CACHE" 2> /dev/null)")
+
 if [ -n "$choice" ]; then
     filename=$(echo "$choice" | cut -d ':' -f 1)
     line=$(echo "$choice" | cut -d ':' -f 2)
+    col=$(echo "$choice" | cut -d ':' -f 3)
 
-    nvim +"$line" "$filename"
+    nvim +"call cursor($line, $col)" "$filename"
 fi
