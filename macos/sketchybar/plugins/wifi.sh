b5b6f76ec2a49ca7f0599eba554db77ef46d9144
rfmajor <rfmajor99@gmail.com>
2025-11-29 20:48:54 +0100

    Disable speedtest

--- macos/sketchybar/plugins/wifi.sh
+++ macos/sketchybar/plugins/wifi.sh
@@ -16,20 +16,20 @@ fi
 
 sketchybar --set "$NAME" icon="$ICON" label="$NETWORK" icon.color="$COLOR"
 
-speedtest=$(speedtest --json)
-if [ -z "$speedtest" ]; then
-    download="--"
-    upload="--"
-else
-    download=$(echo "$speedtest" | jq '.download')
-    upload=$(echo "$speedtest" | jq '.upload')
-
-    download=$(echo "$download" | cut -d '.' -f 1 | tr -d '\n')
-    upload=$(echo "$upload" | cut -d '.' -f 1 | tr -d '\n')
-
-    download=$(($download / 1000000))
-    upload=$(($upload / 1000000))
-fi
-
-sketchybar --set "${NAME}_popup_download" label="$download Mb/s"
-sketchybar --set "${NAME}_popup_upload" label="$upload Mb/s"
+#speedtest=$(speedtest --json)
+#if [ -z "$speedtest" ]; then
+#    download="--"
+#    upload="--"
+#else
+#    download=$(echo "$speedtest" | jq '.download')
+#    upload=$(echo "$speedtest" | jq '.upload')
+#
+#    download=$(echo "$download" | cut -d '.' -f 1 | tr -d '\n')
+#    upload=$(echo "$upload" | cut -d '.' -f 1 | tr -d '\n')
+#
+#    download=$(($download / 1000000))
+#    upload=$(($upload / 1000000))
+#fi
+#
+#sketchybar --set "${NAME}_popup_download" label="$download Mb/s"
+#sketchybar --set "${NAME}_popup_upload" label="$upload Mb/s"
