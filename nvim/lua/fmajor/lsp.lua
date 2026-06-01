f2ddb0a3ad5eb47b88acc4508978f89518c716cd
rfmajor <rfmajor99@gmail.com>
2026-06-01 19:03:58 +0200

    Add global lua options for vim

--- nvim/lua/fmajor/lsp.lua
+++ nvim/lua/fmajor/lsp.lua
@@ -81,8 +81,34 @@ vim.api.nvim_set_hl(0, "DiagnosticFloatingOk", { fg = green, italic = true })
 
 --vim.lsp.config("pyright", {
 --})
---vim.lsp.config("lua_ls", {
---})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
 --vim.lsp.config("bashls", {
 --})
 --vim.lsp.config("ts_ls", {
@@ -95,11 +121,6 @@ vim.api.nvim_set_hl(0, "DiagnosticFloatingOk", { fg = green, italic = true })
 --})
 --vim.lsp.config("clangd", {
 --})
-vim.lsp.config("asm-lsp", {
-    filetypes = {
-        "s", "S", "asm"
-    }
-})
 vim.lsp.enable("pyright")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("bashls")
@@ -108,4 +129,3 @@ vim.lsp.enable("postgres_lsp")
 vim.lsp.enable("terraformls")
 vim.lsp.enable("marksman")
 vim.lsp.enable("clangd")
-vim.lsp.enable("asm-lsp")
