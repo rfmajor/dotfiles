1f806c95085e634cf9f26fca9654a7e8b0ba767d
rfmajor <rfmajor99@gmail.com>
2026-06-08 12:46:36 +0200

    Change formatting

--- nvim/lua/fmajor/conform.lua
+++ nvim/lua/fmajor/conform.lua
@@ -19,25 +19,11 @@ conform.formatters.prettier = {
     append_args = { "--config", os.getenv("HOME") .. "/dotfiles/.prettierrc.json" }
 }
 
-vim.keymap.set({ "n", "v" }, "<leader>f", function()
-    local mode = vim.fn.mode()
-
+vim.keymap.set({ "n" }, "<leader>f", function()
     local opts = {
         async = true,
         lsp_fallback = true,
     }
-
-    -- If in visual mode, calculate selected range
-    if mode == "v" or mode == "V" or mode == "\22" then
-        local start_pos = vim.api.nvim_buf_get_mark(0, "<") -- start of selection
-        local end_pos = vim.api.nvim_buf_get_mark(0, ">")   -- end of selection
-
-        opts.range = {
-            start = { start_pos[1], start_pos[2] },
-            ["end"] = { end_pos[1], end_pos[2] },
-        }
-    end
-
     conform.format(opts)
-end, { desc = "Format selection or whole file" })
+end, { desc = "Format file" })
 
