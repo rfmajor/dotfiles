70c4a02c00aaf565ed571cf13ffeddb162ad141a
rfmajor <rfmajor99@gmail.com>
2026-06-11 21:14:05 +0200

    Update Treesitter config

--- nvim/lua/fmajor/lazy.lua
+++ nvim/lua/fmajor/lazy.lua
@@ -33,7 +33,21 @@ require("lazy").setup({
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
 	},
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					-- Enable treesitter highlighting and disable regex syntax
+					pcall(vim.treesitter.start)
+					-- Enable treesitter-based indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
 	{ "nvim-lua/plenary.nvim" }, -- don't forget to add this one if you don't have it yet!
 	{ "mbbill/undotree" },
 	{ "neovim/nvim-lspconfig" },
